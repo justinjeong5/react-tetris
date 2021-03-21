382ebe601f9ae2532f6195e61c35214bf51e42b3
justinjeong <justin.jeong5@gmail.com>
2021-03-21 13:40:54 +0900

    style(config): eslint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-undef */
+/* eslint-disable quotes */
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "react/jsx-filename-extension": [1, { "extensions": ["js", "jsx"] }],
+        "arrow-parens": ["warn", "as-needed"],
+        "no-console": ["off"],
+        "react/jsx-props-no-spreading": ["warn"],
+        "semi": ["error", "always"],
+        "quotes": ["error", "single"]
+    }
+};
